3443a39c58e63c69a487ff7e895986f4ef95277b
EsdrasC24 <josuequeraleswd2420@gmail.com>
2025-01-29 20:56:45 -0400

    feat: add routes

--- horarios/models.py
+++ horarios/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from asignaturas.models import Asignatura
+
 # Create your models here.
+
+class Horario(models.Model):
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+    dia_semana = models.CharField(max_length=10, choices=[
+        ('Lunes', 'Lunes'),
+        ('Martes', 'Martes'),
+        ('Miércoles', 'Miércoles'),
+        ('Jueves', 'Jueves'),
+        ('Viernes', 'Viernes'),
+        ('Sábado', 'Sábado'),
+        ('Domingo', 'Domingo'),
+    ])
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    aula = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.asignatura} - {self.dia_semana} ({self.hora_inicio} - {self.hora_fin})"
