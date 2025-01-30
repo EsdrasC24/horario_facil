898357359aecc5e82f91551b8ef74cf87334ce7f
EsdrasC24 <josuequeraleswd2420@gmail.com>
2025-01-29 21:44:07 -0400

    feat: add views for estudiantes

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from horarios.models import Horario
 
 # Create your models here.
+
+class Estudiante(models.Model):
+#    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    horarios = models.ManyToManyField(Horario)
+
+    def __str__(self):
+        return self.usuario.username
