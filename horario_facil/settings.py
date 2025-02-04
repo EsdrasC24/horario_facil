03b1d983515b48d6f0c419b21e2db02e39664c0c
EsdrasC24 <josuequeraleswd2420@gmail.com>
2025-02-04 17:50:51 -0400

    fix: add change on settings

--- horario_facil/settings.py
+++ horario_facil/settings.py
@@ -31,20 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+# Aplicaciones personalizadas
+    'asignaturas',
+    'horarios',
+    'estudiantes',
+#
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Aplicaciones personalizadas
-    'asignaturas',
-    'horarios',
-    'estudiantes'
-#    'asignaturas.apps.AsignaturasConfig',  # Registro usando AppConfig
-#    'horarios.apps.HorariosConfig',        # Registro usando AppConfig
-#    'estudiantes.apps.EstudiantesConfig',  # Registro usando AppConfig
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
